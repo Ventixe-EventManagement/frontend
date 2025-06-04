8e1fb7d195d015219b342e396295910d76c4b1b6
Jeanette Mikkelsen <jeanette.mikkelsen@utb.ecutbildning.se>
2025-06-04 10:59:54 +0200

    Add missing plugin-react dependency for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   plugins: [
     react(),
     copy({
-      targets: [{ src: 'staticwebapp.config.json', dest: 'dist' }],
+      targets: [
+        { src: 'staticwebapp.config.json', dest: 'dist' }
+      ],
       hook: 'writeBundle'
     })
   ]
