dfdf5a1b131819cafe0fd667d90ce68f6c5abd47
Jeanette Mikkelsen <jeanette.mikkelsen@utb.ecutbildning.se>
2025-06-05 11:04:09 +0200

    testar bookingservice live

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export const API_URLS = {
   account: 'https://ventixe-accountserviceprovider-cdhuf2azb3end8eb.swedencentral-01.azurewebsites.net',
   auth: 'https://ventixe-authserviceprovider-fjgta2ecdue9cfa6.swedencentral-01.azurewebsites.net',
-  profile: 'https://ventixe-accountprofileserviceprovider-egcfgganfca8epa8.swedencentral-01.azurewebsites.net'
+  profile: 'https://ventixe-accountprofileserviceprovider-egcfgganfca8epa8.swedencentral-01.azurewebsites.net',
+  booking: 'https://ventixe-bookingservice-dzb9cxd0fjdzeqen.swedencentral-01.azurewebsites.net'
+
 };
